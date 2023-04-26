5a6a2294e53837c32ea368088eec702d5a304683
vic m <vm@vics-MacBook-Pro.local>
2023-04-26 19:11:50 +0300

    feat: update todo redux store and display todo under form

--- pages/todo.jsx
+++ pages/todo.jsx
@@ -1,28 +1,40 @@
-import TodoAPI from "@/lib/todo/todo";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateTodo } from "@/lib/features/todos/todoSlice";
+
+import TodoAPI from "@/lib/todo/todo";
 
 const Todo = () => {
   const [todoId, setTodoId] = useState();
-  const [todo, setTodo] = useState();
+  const todo = useSelector((state) => state.todo);
+  const dispatch = useDispatch();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = await TodoAPI.getTodoById(todoId);
-    console.log(data);
-    setTodo(data);
+    dispatch(updateTodo(data));
   };
 
   return (
-    <div>
-      <form method="post" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter todo ID"
-          onChange={(event) => setTodoId(event.target.value)}
-        />
-        <button type="submit">submit</button>
-      </form>
-    </div>
+    <>
+      <div>
+        <form method="post" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter todo ID"
+            onChange={(event) => {
+              setTodoId(event.target.value);
+              console.log(event.target.value);
+            }}
+          />
+          <button type="submit">submit</button>
+        </form>
+        <div>
+          <h2>{todo.title}</h2>
+          <p>{todo.completed ? "Completed" : "Not Completed"}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
