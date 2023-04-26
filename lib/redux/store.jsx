be50acd9f9075b6e3a1c22d204960a217db25af1
vic m <vm@vics-MacBook-Pro.local>
2023-04-26 18:07:12 +0300

    feat: added count slice reducers to the store

--- lib/redux/store.jsx
+++ lib/redux/store.jsx
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import counterReducer from "@/lib/features/counter/counterSlice";
+
 export default configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
