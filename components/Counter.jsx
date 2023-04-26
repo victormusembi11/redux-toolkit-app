a5b3b1b1e2060fce62baf11fb5a38b6022ac4a6b
vic m <vm@vics-MacBook-Pro.local>
2023-04-26 18:08:19 +0300

    feat: increment & decrement counter

---
+++ components/Counter.jsx
@@ -0,0 +1,29 @@
+import { increment } from "@/lib/features/counter/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
