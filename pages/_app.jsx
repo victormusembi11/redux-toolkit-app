cc50b436be26d1dc663384bad9598335c5b05789
vic m <vm@vics-MacBook-Pro.local>
2023-04-26 17:42:57 +0300

    config: wrapped app with redux provider

---
+++ pages/_app.jsx
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+
+import "@/styles/globals.css";
+import store from "@/lib/redux/store";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
