c12e372cde2bbbe299cfe47fef8b9a75b47689c8
vic m <vm@vics-MacBook-Pro.local>
2023-04-27 20:16:13 +0300

    feat: added todos slice to redux store

--- lib/redux/store.jsx
+++ lib/redux/store.jsx
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "@/lib/features/counter/counterSlice";
 import todoSlice from "@/lib/features/todos/todoSlice";
+import todosSlice from "../features/todos/todosSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     todo: todoSlice,
+    todos: todosSlice,
   },
 });
