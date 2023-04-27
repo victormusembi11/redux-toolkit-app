41b6a97663c7ee1d37bda27b9f954aa52adbddbd
vic m <vm@vics-MacBook-Pro.local>
2023-04-27 20:16:41 +0300

    feat: make api call for all todos

--- lib/todo/todo.jsx
+++ lib/todo/todo.jsx
@@ -10,8 +10,19 @@ async function getTodoById(id) {
   console.log("error fetching data");
 }
 
+async function getAllTodos() {
+  const response = await defaultFetch(`/todos/`);
+
+  if (response.status === 200) {
+    return response.data;
+  }
+
+  console.log("error fetching data");
+}
+
 const TodoAPI = {
   getTodoById,
+  getAllTodos,
 };
 
 export default TodoAPI;
