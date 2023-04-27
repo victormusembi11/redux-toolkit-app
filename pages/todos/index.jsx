22c98c2b2de1d87d9af536fb536da22515d0dfd8
vic m <vm@vics-MacBook-Pro.local>
2023-04-27 23:21:21 +0300

    feat: redirect to todo detail page on title click

--- pages/todos/index.jsx
+++ pages/todos/index.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateTodos } from "@/lib/features/todos/todosSlice";
+import Link from "next/link";
 
 import TodoAPI from "@/lib/todo/todo";
 
 const TodoList = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
 
@@ -14,21 +16,28 @@ const TodoList = () => {
       dispatch(updateTodos(data));
     };
     fetchTodos();
+    setIsLoading(false);
   }, [dispatch]);
 
   return (
     <>
-      {todos.map((todo) => {
-        return (
-          <ul key={todo.userId}>
-            <li>{todo.title}</li>
-            <li>
-              <p>{todo.completed ? "Completed" : "Not Completed"}</p>
-            </li>
-            <br />
-          </ul>
-        );
-      })}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        todos.map((todo) => {
+          return (
+            <ul key={todo.userId}>
+              <li>
+                <Link href={`/todos/${todo.id}`}>{todo.title}</Link>
+              </li>
+              <li>
+                <p>{todo.completed ? "Completed" : "Not Completed"}</p>
+              </li>
+              <br />
+            </ul>
+          );
+        })
+      )}
     </>
   );
 };
