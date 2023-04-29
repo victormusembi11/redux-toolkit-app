ad135352dfe0ee5b7b47a8f0d7b0952dab0fc2c5
vwm <victor.musembi11@gmail.com>
2023-04-29 15:30:46 +0300

    todo: wrap app with chakra ui provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import "@/styles/globals.css";
 import store from "@/lib/redux/store";
 
+// TODO: Wrap app with chakra ui provider
+
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
